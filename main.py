05c7d25a2e34fdc006e3bedf3787799e475fe2c1
OfHeikan <ofheikan@gmail.com>
2026-09-15 23:01:22 +0800

    Update configuration values in main.py
    Updated PersonalId, SessionId, and year_semester values. Adjusted weekRange and removed server parameter from download_jsons call.

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ from filter import data_filter
 from toICS import to_ics
 
 # 个人id F12 GetStudentBasicInfoLessonOccupy SMXSJBXXID
-PersonalId = "d85695ed-799c-418f-85ea-6e02d50a7b9b"
+PersonalId = "862d4596-de18-4673-9a4f-c1024f3ecd26"
 
 # cookie ASP.NET_SessionId
-SessionId = "f2i2isiycjtaub4oz1uuum2z"
+SessionId = "35pobiebnvheij0mad01utdb"
 
 # 服务器编号
-server = 3
+#server = 3
 
 # 周数
-weekRange = range(0,21)
+weekRange = range(0,20)
 
 # 学年学期
-year_semester = "20242"
+year_semester = "20261"
 
-download_jsons(weekRange,PersonalId,SessionId, server, year_semester)
+download_jsons(weekRange,PersonalId,SessionId, year_semester)
 data_filter(weekRange)
 to_ics("all_lessons")
